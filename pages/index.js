89adfa809afbefd8aa64753f6a5c21d923265e6d
mdAshik922 <md.ashik0005723@gmail.com>
2022-02-06 23:11:01 +0600

    commit

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
 </div>
 </div>
 
-      <footer >
+      <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
