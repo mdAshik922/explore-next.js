c9e24ec07a2e0ff063ee6d30c75a52cec9da69a0
mdAshik922 <md.ashik0005723@gmail.com>
2022-02-06 23:23:46 +0600

    commit

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
 
 <div>
   <div>
-    <h3><svg className="w-6 h-6 text-cloud-dark dark:text-cloud-light" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path></svg>Documentation</h3>
+    <h3><svg className="w-6 h-6 text-cloud-dark dark:text-cloud-light" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" strokeLinejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path></svg>Documentation</h3>
     <p>We highly recommend you take a look at the Nuxt
        documentation, whether you are new
        or have previous experience with the framework.</p>
