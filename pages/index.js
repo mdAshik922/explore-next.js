2d6e80eff66ab032f97cc8385665a078152c24f0
mdAshik922 <md.ashik0005723@gmail.com>
2022-02-16 14:53:46 +0600

    commit

--- pages/index.js
+++ pages/index.js
@@ -23,13 +23,7 @@ export default function Home() {
 </h2>
 
 <div>
-  <div>
-    <h3><svg className="w-6 h-6 text-cloud-dark dark:text-cloud-light" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path></svg>Documentation</h3>
-    <p>We highly recommend you take a look at the Nuxt
-       documentation, whether you are new
-       or have previous experience with the framework.</p>
-  <a src="https://v3.nuxtjs.org/" target="_blank">Documentation</a>
-  </div>
+ 
 <div>
 
   <h3><svg className="w-6 h-6 text-cloud-dark dark:text-cloud-light" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>Twitter</h3>
